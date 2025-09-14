41f1d2157329f0259faf7dd3248f798c018c5c04
spasdev <samuel.pasquier@gmx.ch>
2025-09-15 01:12:18 +0200

    cont

--- start.sh
+++ start.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
-set -e
 
-# 1. Start the Tailscale daemon in the background.
 /app/tailscaled --tun=userspace-networking --socks5-server=127.0.0.1:1055 &
-sleep 3
-
-# 2. Bring Tailscale up and have it manage DNS and routes.
-/app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=evcc-container --accept-dns=true --accept-routes=true
-
-sleep 10
-
-echo "✅ Tailscale is connected and running."
-
-# 4. Execute the evcc application directly, setting the environment variable
-#    only for this specific command.
-exec env ALL_PROXY=socks5://127.0.0.1:1055/ evcc --config /etc/evcc.yaml
+/app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=cloudrun-evcc --accept-dns=true --accept-routes=true
+echo Tailscale started
+ALL_PROXY=socks5://127.0.0.1:1055/ evcc --config /etc/evcc.yaml
